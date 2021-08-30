0cb8e0474ab35ebfced558f0a57e94562cf0eb06
felipesouza77 <56730668+felipesouza77@users.noreply.github.com>
2021-08-30 20:17:13 -0300

    Update esteira .lua

--- esteira .lua
+++ esteira .lua
@@ -1,7 +1,7 @@
 ------------------- LUA Macros Template. -------------------------------------------------
 lmc.minimizeToTray = true
 lmc_minimize()
-lmc_send_keys('%{F4}')
+
 lmc_load('C:\\Descargas\\luamacros\\Main.lua')
 
 lmc_device_set_name('Esteira','23843949')
